fcc5010a9f92650b49d170cfd3498531125e9b0a
Osman Bay <bayosman342@gmail.com>
2025-12-31 02:53:34 +0300

    feat(session): implement session management with express-session and connect-mongo

--- server/server.ts
+++ server/server.ts
@@ -2,14 +2,36 @@ import express, { Request, Response } from 'express';
 import "dotenv/config";
 import cors from "cors";
 import connectToDatabase from './config/database.js';
+import session from "express-session"
+import MongoStore from "connect-mongo"
+
+declare module 'express-session' {
+  interface SessionData {
+    isLoggedIn: boolean;
+    userId: string;
+  }
+}
 
 await connectToDatabase();
 
 const app = express();
 
-
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "http://localhost:3000"],
+  credentials: true,
+}));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET as string,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 },   // 7 days
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI as string,
+    collectionName: "sessions",
+  })
+}))
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Server is Live!');
